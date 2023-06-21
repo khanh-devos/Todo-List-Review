e9c000983d60a8bb7c07be07efe9a48930b458b6
H-Ramalan <talktoramalan@gmail.com>
2023-06-21 14:49:09 -0700

    add test for deleteTasks

--- test/part1.js
+++ test/part1.js
@@ -18,5 +18,25 @@ export const addNew = (description) => {
     const check = data.getData();
     return check[check.length - 1];
   }
-  throw new Error('task cannot be an empty string');
+  throw new Error("task cannot be an empty string");
+};
+
+export const deleteTasks = (checks) => {
+  // const checks = document.querySelectorAll('input[type="checkbox"]');
+  const data = new Data();
+  const checkeds = [];
+  checks.forEach((e) => {
+    if (e.checked) checkeds.push(e.name);
+  });
+
+  if (checkeds.length > 0) {
+    data.deleteArrOfIndex(checkeds);
+  }
+
+  // for testing
+  if (checkeds.length > 0) {
+    const checkTasks = data.getData();
+    return checkTasks;
+  }
+  throw new Error("no task completed");
 };
--- test/part1.test.js
+++ test/part1.test.js
@@ -1,16 +1,38 @@
 import * as funcs from "./part1.js";
 
-describe('part 1', () => {
-    test('addNew', () => {
-      expect(funcs.addNew('task4')).toStrictEqual({
-        index: 4,
-        description: 'task4',
-        completed: false,
-      });
-  
-      expect(() => funcs.addNew('')).toThrow(/task cannot be an empty string/);
-    
+describe("part 1", () => {
+  test("addNew", () => {
+    expect(funcs.addNew("task4")).toStrictEqual({
+      index: 4,
+      description: "task4",
+      completed: false,
     });
-  
-    
+
+    expect(() => funcs.addNew("")).toThrow(/task cannot be an empty string/);
   });
+
+  test("deleteTasks", () => {
+    expect(
+      funcs.deleteTasks([
+        {
+          checked: true,
+          name: "1",
+        },
+        {
+          checked: true,
+          name: "2",
+        },
+        {
+          checked: false,
+          name: "3",
+        },
+      ])
+    ).toStrictEqual([
+      {
+        index: 1,
+        description: "task3",
+        completed: false,
+      },
+    ]);
+  });
+});
